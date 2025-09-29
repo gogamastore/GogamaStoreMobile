cbffadb5de7a704f10028f5f1eedca0c4a3121a4
enerinsanmulia <enerinsanmulia@gmail.com>
2025-09-29 19:53:58 +0000

    Final versi 4.3.2  build 10

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     // START: FlutterFire Configuration
@@ -9,13 +11,22 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+// Corrected logic to read properties
+val keyProperties = Properties()
+// The key.properties file is located in the `android` folder, which is the rootProject for this gradle build.
+val keyPropertiesFile = rootProject.file("key.properties") 
+if (keyPropertiesFile.exists()) {
+    keyPropertiesFile.inputStream().use { input ->
+        keyProperties.load(input)
+    }
+}
+
 android {
     namespace = "store.gogama.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        // FIX: Enable core library desugaring to support modern Java features on older Android versions.
         isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -23,6 +34,19 @@ android {
 
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+    
+    signingConfigs {
+        create("release") {
+            // The keystore file is in `android/app`
+            val storeFilePath = keyProperties.getProperty("storeFile")
+            if (storeFilePath != null) {
+                storeFile = rootProject.file("app/$storeFilePath")
+            }
+            storePassword = keyProperties.getProperty("storePassword")
+            keyAlias = keyProperties.getProperty("keyAlias")
+            keyPassword = keyProperties.getProperty("keyPassword")
+        }
     }
 
     defaultConfig {
@@ -36,13 +60,12 @@ android {
 
     buildTypes {
         release {
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
 
 dependencies {
-    // Add the dependency for core library desugaring
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
 
